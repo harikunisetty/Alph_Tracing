507bf88fcfe7a53075d9b40ace6f13775549b201
harikunisetty <86394723+harikunisetty@users.noreply.github.com>
2021-12-15 10:19:27 +0530

    update

---
+++ Assets/English Tracing Book/Scripts/Game/EscapeEvent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeEvent : MonoBehaviour
+{
+		
+		public string sceneName;
+		public bool leaveTheApplication;
+		public bool loadShapesManagerSceneName;
+
+		public void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.Escape)) {
+						OnEscapeClick ();
+				}
+		}
+
+	
+		public void OnEscapeClick ()
+		{
+				if (leaveTheApplication) {
+						GameObject exitConfirmDialog = GameObject.Find ("ExitConfirmDialog");
+						if (exitConfirmDialog != null) {
+								Dialog exitDialogComponent = exitConfirmDialog.GetComponent<Dialog> ();
+								if (!exitDialogComponent.animator.GetBool ("On")) {
+										exitDialogComponent.Show ();
+										//AdsManager.instance.ShowAdvertisment (AdsManager.AdAPI.AdEvent.Event.ON_SHOW_EXIT_DIALOG);
+								}
+						}
+				} else {
+					if (loadShapesManagerSceneName) {
+						StartCoroutine(SceneLoader.LoadSceneAsync (GameObject.Find(ShapesManager.shapesManagerReference).GetComponent<ShapesManager>().sceneName));
+					} else {
+						StartCoroutine (SceneLoader.LoadSceneAsync (sceneName));
+					}
+				}
+		}
+}
